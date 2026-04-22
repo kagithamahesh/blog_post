5b3694a058d5b7f9417a8cdf2ae96d00a2d8357c
Mahesh <kagithacm237@gmail.com>
2026-04-22 07:02:47 +0530

    fixed the error

--- main.go
+++ main.go
@@ -1,22 +1,21 @@
 package main
 
 import (
-    "log"
-    "your_project/config" // Import the config package
+	"BlogPost/config"
+	"log"
 )
 
 func main() {
-    // Initialize DB connection
-    db := config.ConnectDB()
-    if db == nil {
-        log.Fatal("Failed to connect to the database")
-    }
+	// Initialize DB connection
+	db := config.ConnectDB()
+	if db == nil {
+		log.Fatal("Failed to connect to the database")
+	}
 
-    // Initialize Redis connection
-    redis := config.ConnectRedis()
-    if redis == nil {
-        log.Fatal("Failed to connect to Redis")
-    }
-    
-    // Rest of your code...
+	// Initialize Redis connection
+	redis := config.ConnectRedis()
+	if redis == nil {
+		log.Fatal("Failed to connect to Redis")
+	}
+
 }
